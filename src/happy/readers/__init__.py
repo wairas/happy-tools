0a7f85bbad9bdc18c6479e1eef683869b0bc47ec
Peter Reutemann <fracpete@gmail.com>
2023-10-10 13:18:17 +1300

    removed obsolete code

--- src/happy/readers/__init__.py
+++ src/happy/readers/__init__.py
@@ -2,5 +2,4 @@ from ._envi_reader import EnviReader
 from ._happy_reader import HappyReader
 from ._json_reader import JsonReader
 from ._mat_reader import MatReader
-from ._numpy_reader import NumpyReader
 from ._spectra_reader import SpectraReader
