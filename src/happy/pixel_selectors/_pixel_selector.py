a32103c00d5827e9ea1aef47e2f3a4090a666d8e
Peter Reutemann <fracpete@gmail.com>
2023-10-12 11:40:00 +1300

    added parse_pixel_selector class method to PixelSelector

--- src/happy/pixel_selectors/_pixel_selector.py
+++ src/happy/pixel_selectors/_pixel_selector.py
@@ -1,7 +1,8 @@
 import abc
 
 from happy.base.core import ConfigurableObject
-from seppl import Plugin
+from happy.base.registry import REGISTRY
+from seppl import Plugin, split_args, split_cmdline, args_to_objects
 
 
 class PixelSelector(ConfigurableObject, Plugin, abc.ABC):
@@ -14,3 +15,17 @@ class PixelSelector(ConfigurableObject, Plugin, abc.ABC):
 
     def select_pixels(self, happy_data, n=None):
         raise NotImplementedError()
+
+    @classmethod
+    def parse_pixel_selector(cls, cmdline):
+        """
+        Splits the command-line, parses the arguments, instantiates and returns the pixel selectro.
+
+        :param cmdline: the command-line to process
+        :type cmdline: str
+        :return: the pixel selector
+        :rtype: PixelSelector
+        """
+        plugins = REGISTRY.pixel_selectors()
+        args = split_args(split_cmdline(cmdline), plugins.keys())
+        return args_to_objects(args, plugins, allow_global_options=False)
