d632d78b63a88df1e02b0cadd35eb4e936428682
Peter Reutemann <fracpete@gmail.com>
2023-11-20 16:51:54 +1300

    added dir-tree-with-data output format which converts the scan to happy data as well (black and white ref can be applied during that process)

--- README.md
+++ README.md
@@ -233,11 +233,15 @@ optional arguments:
 ```
 
 
-### OPEX to ENVI
+### OPEX to HAPPy
 
 ```
-usage: happy-opex2happy [-h] -i DIR [DIR ...] [-o DIR] -f {flat,dir-tree} -l
-                        LABELS [-I] [-n] [-v]
+usage: happy-opex2happy [-h] -i DIR [DIR ...] [-r] [-o DIR] -f
+                        {flat,dir-tree,dir-tree-with-data} -l LABELS
+                        [--black_ref_locator LOCATOR]
+                        [--black_ref_method METHOD]
+                        [--white_ref_locator LOCATOR]
+                        [--white_ref_method METHOD] [-I] [-n] [-v]
 
 Turns annotations (PNG and OPEX JSON) into Happy ENVI format.
 
@@ -245,18 +249,37 @@ optional arguments:
   -h, --help            show this help message and exit
   -i DIR [DIR ...], --input_dir DIR [DIR ...]
                         Path to the PNG/OPEX files (default: None)
+  -r, --recursive       whether to look for OPEX files recursively (default:
+                        False)
   -o DIR, --output_dir DIR
                         The directory to store the fake RGB PNG images instead
                         of alongside the HSI images. (default: None)
-  -f {flat,dir-tree}, --output_format {flat,dir-tree}
+  -f {flat,dir-tree,dir-tree-with-data}, --output_format {flat,dir-tree,dir-tree-with-data}
                         Defines how to store the data in the output directory.
                         (default: flat)
   -l LABELS, --labels LABELS
                         The comma-separated list of object labels to export
                         ('Background' is automatically added). (default: None)
+  --black_ref_locator LOCATOR
+                        the reference locator scheme to use for locating black
+                        references, eg rl-manual; requires: dir-tree-with-data
+                        (default: None)
+  --black_ref_method METHOD
+                        the black reference method to use for applying black
+                        references, eg br-same-size; requires: dir-tree-with-
+                        data (default: None)
+  --white_ref_locator LOCATOR
+                        the reference locator scheme to use for locating
+                        whites references, eg rl-manual; requires: dir-tree-
+                        with-data (default: None)
+  --white_ref_method METHOD
+                        the white reference method to use for applying white
+                        references, eg wr-same-size; requires: dir-tree-with-
+                        data (default: None)
   -I, --include_input   whether to copy the PNG/JSON file across to the output
                         dir (default: False)
-  -n, --dry_run         whether to omit saving the PNG images (default: False)
+  -n, --dry_run         whether to omit generating any data or creating
+                        directories (default: False)
   -v, --verbose         whether to be more verbose with the output (default:
                         False)
 ```
--- src/happy/console/opex_to_happy/generate.py
+++ src/happy/console/opex_to_happy/generate.py
@@ -7,46 +7,188 @@ import traceback
 
 import numpy as np
 from spectral import envi
+import spectral.io.envi as envi_load
 from PIL import Image, ImageDraw
 from opex import ObjectPredictions
+from happy.data import HappyData
+from happy.writers import HappyWriter
+from happy.data.ref_locator import AbstractReferenceLocator
+from happy.data.white_ref import AbstractWhiteReferenceMethod
+from happy.data.black_ref import AbstractBlackReferenceMethod
 
 OUTPUT_FORMAT_FLAT = "flat"
 OUTPUT_FORMAT_DIRTREE = "dir-tree"
+OUTPUT_FORMAT_DIRTREE_WITH_DATA = "dir-tree-with-data"
 OUTPUT_FORMATS = [
     OUTPUT_FORMAT_FLAT,
     OUTPUT_FORMAT_DIRTREE,
+    OUTPUT_FORMAT_DIRTREE_WITH_DATA,
 ]
 
 
-def convert(path_png, path_ann, output_dir, output_format=OUTPUT_FORMAT_FLAT, labels=None,
+DEFAULT_REGION_ID = "1"
+
+
+def locate_opex(input_dirs, recursive, verbose, opex_files):
+    """
+    Locates the PNG/OPEX JSON pairs.
+
+    :param input_dirs: the input dir(s) to traverse
+    :type input_dirs: str or list
+    :param recursive: whether to look for OPEX files recursively
+    :type recursive: bool
+    :param verbose: whether to be more verbose with the output
+    :type verbose: bool
+    :param opex_files: for collecting the OPEX JSON files
+    :type opex_files: list
+    """
+    if isinstance(input_dirs, str):
+        input_dirs = [input_dirs]
+
+    for input_dir in input_dirs:
+        if verbose:
+            print("Entering: %s" % input_dir)
+
+        for f in os.listdir(input_dir):
+            full = os.path.join(input_dir, f)
+
+            # directory?
+            if os.path.isdir(full):
+                if recursive:
+                    locate_opex(full, recursive, verbose, opex_files)
+                    if verbose:
+                        print("Back in: %s" % input_dir)
+                else:
+                    continue
+
+            if not f.lower().endswith(".json"):
+                continue
+
+            ann_path = os.path.join(input_dir, f)
+            prefix = os.path.splitext(ann_path)[0]
+            img_path = prefix + ".png"
+            if not os.path.exists(img_path):
+                if verbose:
+                    print("No annotation JSON/PNG pair for: %s" % (prefix + ".*"))
+                continue
+            else:
+                opex_files.append(ann_path)
+
+
+def get_sample_id(path):
+    """
+    Returns the sample ID determined from the specified path.
+
+    :param path: the path to get the sample ID from
+    :type path: str
+    :return: the sample ID
+    :rtype: str
+    """
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def envi_to_happy(path_ann, output_dir, black_ref_locator=None, black_ref_method=None,
+                  white_ref_locator=None, white_ref_method=None, dry_run=False, verbose=False):
+    """
+    Converts the envi data into happy format.
+
+    :param path_ann: the path of the annotation JSON file (to locate other data from)
+    :type path_ann: str
+    :param output_dir: the directory to store the happy data in
+    :type output_dir: str
+    :param black_ref_locator: reference locator for black reference files, ignored if None
+    :type black_ref_locator: AbstractReferenceLocator
+    :param black_ref_method: the black reference method to apply, ignored if None
+    :type black_ref_method: AbstractBlackReferenceMethod
+    :param white_ref_locator: reference locator for white reference files, ignored if None
+    :type white_ref_locator: AbstractReferenceLocator
+    :param white_ref_method: the white reference method to apply, ignored if None
+    :type white_ref_method: AbstractWhiteReferenceMethod
+    :param dry_run: whether to omit saving data/creating dirs
+    :type dry_run: bool
+    :param verbose: whether to be more verbose with the output
+    :type verbose: bool
+    """
+    if verbose:
+        print("  --> converting envi to happy format")
+
+    path_hdr = os.path.splitext(path_ann)[0] + ".hdr"
+    if not os.path.exists(path_hdr):
+        print("    --> not found: %s" % path_hdr)
+        return
+
+    if verbose:
+        print("    --> loading: %s" % path_hdr)
+    scan = envi_load.open(path_hdr).load()
+
+    if black_ref_locator is not None:
+        black_ref_file = black_ref_locator.locate(path_hdr)
+        if black_ref_file is not None:
+            if os.path.exists(black_ref_file):
+                if verbose:
+                    print("    --> loading black ref: %s" % black_ref_file)
+                black_ref = envi_load.open(path_hdr).load()
+                black_ref_method.reference = black_ref
+                scan = black_ref_method.apply(scan)
+            else:
+                print("    --> black ref file not found: %s" % black_ref_file)
+
+    if white_ref_locator is not None:
+        white_ref_file = white_ref_locator.locate(path_hdr)
+        if white_ref_file is not None:
+            if os.path.exists(white_ref_file):
+                if verbose:
+                    print("    --> loading white ref: %s" % white_ref_file)
+                white_ref = envi_load.open(path_hdr).load()
+                white_ref_method.reference = white_ref
+                scan = white_ref_method.apply(scan)
+        else:
+            print("    --> white ref file not found: %s" % white_ref_file)
+
+    data = HappyData(get_sample_id(path_ann), DEFAULT_REGION_ID, scan, {}, {})
+    if not dry_run:
+        if verbose:
+            print("    --> writing happy data")
+        writer = HappyWriter(base_dir=output_dir)
+        writer.write_data(data)
+
+
+def convert(path_ann, path_png, output_dir, output_format=OUTPUT_FORMAT_FLAT, labels=None,
+            black_ref_locator=None, black_ref_method=None, white_ref_locator=None, white_ref_method=None,
             include_input=False, dry_run=False, verbose=False):
     """
     Converts the specified file.
 
-    :param path_png: the PNG image
-    :type path_png: str
     :param path_ann: the OPEX JSON file
     :type path_ann: str
+    :param path_png: the PNG image
+    :type path_png: str
     :param output_dir: where to store the ENVI data
     :type output_dir: str
     :param output_format: how to store the generated ENVI images
     :type output_format: str
     :param labels: the list of labels to transfer from OPEX into ENVI
     :type labels: list
+    :param black_ref_locator: reference locator for black reference files, ignored if None
+    :type black_ref_locator: AbstractReferenceLocator
+    :param black_ref_method: the black reference method to apply, ignored if None
+    :type black_ref_method: AbstractBlackReferenceMethod
+    :param white_ref_locator: reference locator for white reference files, ignored if None
+    :type white_ref_locator: AbstractReferenceLocator
+    :param white_ref_method: the white reference method to apply, ignored if None
+    :type white_ref_method: AbstractWhiteReferenceMethod
     :param include_input: whether to copy the PNG/JSON files into the output directory as well
     :type include_input: bool
-    :param dry_run: whether to omit saving the PNG images
-    :type dry_run: bool
-    :param dry_run: whether to omit saving the PNG images
+    :param dry_run: whether to omit saving data/creating dirs
     :type dry_run: bool
     :param verbose: whether to be more verbose with the output
     :type verbose: bool
     """
     if verbose:
-        print("- %s" % path_png)
+        print("- %s" % path_ann)
 
     # determine actual output directory
-    sample_id = os.path.splitext(os.path.basename(path_png))[0]
+    sample_id = os.path.splitext(os.path.basename(path_ann))[0]
     if output_format == OUTPUT_FORMAT_FLAT:
         output_path = output_dir
         # output files
@@ -54,8 +196,8 @@ def convert(path_png, path_ann, output_dir, output_format=OUTPUT_FORMAT_FLAT, la
         output_png = os.path.join(output_path, sample_id + ".png")
         output_ann = os.path.join(output_path, sample_id + ".json")
         output_labels = os.path.join(output_path, sample_id + "-labels.json")
-    elif output_format == OUTPUT_FORMAT_DIRTREE:
-        output_path = os.path.join(output_dir, sample_id, "1")
+    elif (output_format == OUTPUT_FORMAT_DIRTREE) or (output_format == OUTPUT_FORMAT_DIRTREE_WITH_DATA):
+        output_path = os.path.join(output_dir, sample_id, DEFAULT_REGION_ID)
         # output files
         output_envi = os.path.join(output_path, "mask.hdr")
         output_png = os.path.join(output_path, sample_id + ".png")
@@ -63,6 +205,14 @@ def convert(path_png, path_ann, output_dir, output_format=OUTPUT_FORMAT_FLAT, la
         output_labels = os.path.join(output_path, "labels.json")
     else:
         raise Exception("Unhandled output format: %s" % output_format)
+    if verbose:
+        print("  --> output dir: %s" % output_path)
+
+    if output_format == OUTPUT_FORMAT_DIRTREE_WITH_DATA:
+        envi_to_happy(path_ann, output_dir,
+                      black_ref_locator=black_ref_locator, black_ref_method=black_ref_method,
+                      white_ref_locator=white_ref_locator, white_ref_method=white_ref_method,
+                      dry_run=dry_run, verbose=verbose)
 
     # get dimensions
     img = Image.open(path_png)
@@ -87,16 +237,16 @@ def convert(path_png, path_ann, output_dir, output_format=OUTPUT_FORMAT_FLAT, la
             poly = [tuple(x) for x in obj.polygon.points]
             draw.polygon(poly, fill=label_map[obj.label])
 
-    if verbose:
-        print("  --> %s" % output_path)
     if not dry_run:
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
         # copy input?
         if include_input:
-            shutil.copy(path_png, output_png)
+            if verbose:
+                print("    --> copying JSON/PNG")
             shutil.copy(path_ann, output_ann)
+            shutil.copy(path_png, output_png)
 
         # label map/wavelengths
         wavelengths = [0]
@@ -104,15 +254,20 @@ def convert(path_png, path_ann, output_dir, output_format=OUTPUT_FORMAT_FLAT, la
         for k in label_map:
             reverse_label_map[str(label_map[k])] = k
             wavelengths.append(label_map[k])
+        if verbose:
+            print("    --> writing label map: %s" % output_labels)
         with open(output_labels, "w") as fp:
             json.dump(reverse_label_map, fp, indent=2)
 
         # envi mask
+        if verbose:
+            print("    --> writing envi mask: %s" % output_envi)
         envi.save_image(output_envi, np.array(img), dtype=np.uint8, force=True, interleave='BSQ',
                         metadata={'wavelength': wavelengths})
 
 
-def generate(input_dirs, output_dir, output_format=OUTPUT_FORMAT_FLAT, labels=None,
+def generate(input_dirs, output_dir, recursive=False, output_format=OUTPUT_FORMAT_FLAT, labels=None,
+             black_ref_locator=None, black_ref_method=None, white_ref_locator=None, white_ref_method=None,
              include_input=False, dry_run=False, verbose=False):
     """
     Generates fake RGB images from the HSI images found in the specified directories.
@@ -121,10 +276,20 @@ def generate(input_dirs, output_dir, output_format=OUTPUT_FORMAT_FLAT, labels=No
     :type input_dirs: str or list
     :param output_dir: the (optional) output directory to place the generated PNG images in instead of alongside HSI images
     :type output_dir: str
+    :param recursive: whether to look for OPEX files recursively
+    :type recursive: bool
     :param output_format: how to store the generated ENVI images
     :type output_format: str
     :param labels: the list of labels to transfer from OPEX into ENVI
     :type labels: list
+    :param black_ref_locator: reference locator for black reference files, ignored if None
+    :type black_ref_locator: str
+    :param black_ref_method: the black reference method to apply, ignored if None
+    :type black_ref_method: str
+    :param white_ref_locator: reference locator for white reference files, ignored if None
+    :type white_ref_locator: str
+    :param white_ref_method: the white reference method to apply, ignored if None
+    :type white_ref_method: str
     :param include_input: whether to copy the PNG/JSON files into the output directory as well
     :type include_input: bool
     :param dry_run: whether to omit saving the PNG images
@@ -136,29 +301,40 @@ def generate(input_dirs, output_dir, output_format=OUTPUT_FORMAT_FLAT, labels=No
     if output_format not in OUTPUT_FORMATS:
         raise Exception("Unknown output format: %s" % output_format)
 
-    if isinstance(input_dirs, str):
-        input_dirs = [input_dirs]
+    # applying black/white ref?
+    if output_format == OUTPUT_FORMAT_DIRTREE_WITH_DATA:
+        if black_ref_locator is None:
+            black_ref_method = None
+        if black_ref_method is None:
+            black_ref_locator = None
+        if white_ref_locator is None:
+            white_ref_method = None
+        if white_ref_method is None:
+            white_ref_locator = None
 
-    if isinstance(labels, str):
-        labels = [x.strip() for x in labels.split(",")]
+        if black_ref_locator is not None:
+            black_ref_locator = AbstractReferenceLocator.parse_locator(black_ref_locator)
+        if black_ref_method is not None:
+            black_ref_method = AbstractBlackReferenceMethod.parse_method(black_ref_method)
+        if white_ref_locator is not None:
+            white_ref_locator = AbstractReferenceLocator.parse_locator(white_ref_locator)
+        if white_ref_method is not None:
+            white_ref_method = AbstractWhiteReferenceMethod.parse_method(white_ref_method)
+    else:
+        black_ref_locator = None
+        black_ref_method = None
+        white_ref_method = None
+        white_ref_locator = None
 
-    # TODO recursive
-    for input_dir in input_dirs:
-        print("Entering: %s" % input_dir)
+    ann_paths = []
+    locate_opex(input_dirs, recursive, verbose, ann_paths)
 
-        for f in os.listdir(input_dir):
-            if not f.lower().endswith(".png"):
-                continue
-
-            img_path = os.path.join(input_dir, f)
-            ann_path = os.path.splitext(img_path)[0] + ".json"  # TODO don't read labels.json
-            if not os.path.exists(ann_path):
-                if verbose:
-                    print("No annotation for: %s" % img_path)
-                continue
-
-            convert(img_path, ann_path, output_dir=output_dir, output_format=output_format,
-                    labels=labels, include_input=include_input, dry_run=dry_run, verbose=verbose)
+    for ann_path in ann_paths:
+        img_path = os.path.splitext(ann_path)[0] + ".png"
+        convert(ann_path, img_path, output_dir=output_dir, output_format=output_format,
+                black_ref_locator=black_ref_locator, black_ref_method=black_ref_method,
+                white_ref_locator=white_ref_locator, white_ref_method=white_ref_method,
+                labels=labels, include_input=include_input, dry_run=dry_run, verbose=verbose)
 
 
 def main(args=None):
@@ -173,15 +349,22 @@ def main(args=None):
         prog="happy-opex2happy",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--input_dir", nargs="+", metavar="DIR", type=str, help="Path to the PNG/OPEX files", required=True)
+    parser.add_argument("-r", "--recursive", action="store_true", help="whether to look for OPEX files recursively", required=False)
     parser.add_argument("-o", "--output_dir", type=str, metavar="DIR", help="The directory to store the fake RGB PNG images instead of alongside the HSI images.", required=False)
     parser.add_argument("-f", "--output_format", choices=OUTPUT_FORMATS, default=OUTPUT_FORMAT_FLAT, help="Defines how to store the data in the output directory.", required=True)
     parser.add_argument("-l", "--labels", type=str, help="The comma-separated list of object labels to export ('Background' is automatically added).", required=True)
+    parser.add_argument("--black_ref_locator", metavar="LOCATOR", help="the reference locator scheme to use for locating black references, eg rl-manual; requires: " + OUTPUT_FORMAT_DIRTREE_WITH_DATA, default=None, required=False)
+    parser.add_argument("--black_ref_method", metavar="METHOD", help="the black reference method to use for applying black references, eg br-same-size; requires: " + OUTPUT_FORMAT_DIRTREE_WITH_DATA, default=None, required=False)
+    parser.add_argument("--white_ref_locator", metavar="LOCATOR", help="the reference locator scheme to use for locating whites references, eg rl-manual; requires: " + OUTPUT_FORMAT_DIRTREE_WITH_DATA, default=None, required=False)
+    parser.add_argument("--white_ref_method", metavar="METHOD", help="the white reference method to use for applying white references, eg wr-same-size; requires: " + OUTPUT_FORMAT_DIRTREE_WITH_DATA, default=None, required=False)
     parser.add_argument("-I", "--include_input", action="store_true", help="whether to copy the PNG/JSON file across to the output dir", required=False)
-    parser.add_argument("-n", "--dry_run", action="store_true", help="whether to omit saving the PNG images", required=False)
+    parser.add_argument("-n", "--dry_run", action="store_true", help="whether to omit generating any data or creating directories", required=False)
     parser.add_argument("-v", "--verbose", action="store_true", help="whether to be more verbose with the output", required=False)
     parsed = parser.parse_args(args=args)
     generate(parsed.input_dir, parsed.output_dir,
-             output_format=parsed.output_format, labels=parsed.labels,
+             recursive=parsed.recursive, output_format=parsed.output_format, labels=parsed.labels.split(","),
+             black_ref_locator=parsed.black_ref_locator, black_ref_method=parsed.black_ref_method,
+             white_ref_locator=parsed.white_ref_locator, white_ref_method=parsed.white_ref_method,
              include_input=parsed.include_input, dry_run=parsed.dry_run, verbose=parsed.verbose)
 
 
