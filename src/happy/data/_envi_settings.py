79f5d90d91ee157a3720465ed1e33bb43004a7d9
Peter Reutemann <fracpete@gmail.com>
2023-11-22 13:36:24 +1300

    temporarily reverted

--- src/happy/data/_envi_settings.py
+++ src/happy/data/_envi_settings.py
@@ -5,4 +5,4 @@ def configure_envi_settings():
     """
     Adjusts some ENVI settings.
     """
-    spectral.settings.envi_support_nonlowercase_params = True
+    spectral.settings.envi_support_nonlowercase_params = False
