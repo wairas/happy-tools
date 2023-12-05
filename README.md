d4493b61559b848a65b6062dbabc9aecadee6dbd
Peter Reutemann <fracpete@gmail.com>
2023-12-06 11:20:59 +1300

    happy-opex-labels now uses sub-parsers; added update-labels sub-command for updating labels in OPEX files

--- README.md
+++ README.md
@@ -331,11 +331,17 @@ optional arguments:
 ### OPEX labels
 
 ```
-usage: happy-opex-labels [-h] -i INPUT [INPUT ...] [-r] [-a {list-labels}]
-                         [-o OUTPUT_FILE]
+usage: happy-opex-labels [-h] -i INPUT [INPUT ...] [-r]
                          [-V {DEBUG,INFO,WARNING,ERROR,CRITICAL}]
+                         {list-labels,update-labels} ...
 
 Performs actions on OPEX JSON files that it locates.
+
+positional arguments:
+  {list-labels,update-labels}
+                        sub-command help
+    list-labels         Lists the labels in the located files
+    update-labels       Updates the labels using the specified label mapping
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -343,11 +349,6 @@ optional arguments:
                         The dir(s) with the OPEX JSON files (default: None)
   -r, --recursive       Whether to search the directory recursively (default:
                         False)
-  -a {list-labels}, --action {list-labels}
-                        The action to perform (default: list-labels)
-  -o OUTPUT_FILE, --output_file OUTPUT_FILE
-                        Path to the output file; outputs to stdout if omitted
-                        (default: None)
   -V {DEBUG,INFO,WARNING,ERROR,CRITICAL}, --logging_level {DEBUG,INFO,WARNING,ERROR,CRITICAL}
                         The logging level to use. (default: WARN)
 ```
--- src/happy/console/opex_labels/generate.py
+++ src/happy/console/opex_labels/generate.py
@@ -11,9 +11,14 @@ from opex import ObjectPredictions
 PROG = "happy-opex-labels"
 
 
+LABEL_MAPPING_FORMAT = "old_label<TAB>new_label"
+
+
 ACTION_LIST_LABELS = "list-labels"
+ACTION_UPDATE_LABELS = "update-labels"
 ACTIONS = [
     ACTION_LIST_LABELS,
+    ACTION_UPDATE_LABELS,
 ]
 
 
@@ -48,6 +53,45 @@ def list_labels(opex_files, output_file):
         print("\n".join(labels))
 
 
+def update_labels(opex_files, mapping, dry_run, output_file):
+    """
+    Updates the labels stored in the opex files using the specified label mapping (old -> new).
+
+    :param opex_files: the OPEX files to update
+    :type opex_files: list
+    :param mapping: the label mapping (old -> new)
+    :type mapping: dict
+    :param dry_run: whether to perform a dry-run, ie not save affected files
+    :type dry_run: bool
+    :param output_file: the file to store the affected files in , uses stdout if None
+    :type output_file: str
+    """
+    affected_files = []
+    for opex_file in opex_files:
+        is_affected = False
+        preds = None
+        try:
+            logger.info("Processing: %s" % opex_file)
+            preds = ObjectPredictions.load_json_from_file(opex_file)
+            for obj in preds.objects:
+                if obj.label in mapping:
+                    is_affected = True
+                    obj.label = mapping[obj.label]
+        except:
+            logger.exception("Failed to load: %s" % opex_file)
+        if is_affected:
+            affected_files.append(opex_file)
+            if not dry_run and (preds is not None):
+                preds.save_json_to_file(opex_file)
+
+    if not dry_run and (output_file is not None):
+        logger.info("Saving affected files to: %s" % output_file)
+        with open(output_file, "w") as fp:
+            fp.write("\n".join(affected_files))
+    else:
+        print("\n".join(affected_files))
+
+
 def main():
     init_app()
     parser = argparse.ArgumentParser(
@@ -56,9 +100,19 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input', type=str, help='The dir(s) with the OPEX JSON files', required=True, nargs="+")
     parser.add_argument('-r', '--recursive', action="store_true", help='Whether to search the directory recursively', default=False, required=False)
-    parser.add_argument('-a', '--action', choices=ACTIONS, default=ACTION_LIST_LABELS, help='The action to perform', required=False)
-    parser.add_argument('-o', '--output_file', type=str, help='Path to the output file; outputs to stdout if omitted', default=None)
     add_logging_level(parser, short_opt="-V")
+    subparsers = parser.add_subparsers(help='sub-command help', dest="command")
+
+    # action: list-labels
+    parser_list = subparsers.add_parser(ACTION_LIST_LABELS, help='Lists the labels in the located files')
+    parser_list.add_argument('-o', '--output_file', type=str, help='Path to the output file for storing the labels; outputs to stdout if omitted', default=None)
+
+    # action: update-labels
+    parser_update = subparsers.add_parser(ACTION_UPDATE_LABELS, help='Updates the labels using the specified label mapping')
+    parser_update.add_argument('-m', '--label_mapping', type=str, help='Path to the label mapping file when updating labels; format: ' + LABEL_MAPPING_FORMAT, default=None, required=False)
+    parser_update.add_argument('-n', '--dry_run', action="store_true", help='Whether to skip saving affected files', default=False, required=False)
+    parser_update.add_argument('-o', '--output_file', type=str, help='Path to the output file for storing the affected files; outputs to stdout if omitted', default=None)
+
     parsed = parser.parse_args()
     set_logging_level(logger, parsed.logging_level)
 
@@ -67,8 +121,28 @@ def main():
     locate_opex(parsed.input, opex_files, recursive=parsed.recursive, require_png=False, logger=logger)
 
     # perform action
-    if parsed.action == ACTION_LIST_LABELS:
+    if parsed.command == ACTION_LIST_LABELS:
         list_labels(opex_files, parsed.output_file)
+    elif parsed.command == ACTION_UPDATE_LABELS:
+        if parsed.label_mapping is None:
+            raise Exception("No label mapping file provided!")
+        mapping = dict()
+        with open(parsed.label_mapping, "r") as fp:
+            for line in fp.readlines():
+                line = line.strip()
+                if "\t" in line:
+                    parts = line.split("\t")
+                    if len(parts) == 2:
+                        if parts[0] == parts[1]:
+                            logger.info("Skipping identical old/new labels: %s == %s" % (parts[0], parts[1]))
+                        else:
+                            mapping[parts[0]] = parts[1]
+                    else:
+                        logger.warning("Expected format '%s' but got: %s" % (LABEL_MAPPING_FORMAT, line))
+        if len(mapping) == 0:
+            logger.warning("Label mapping is empty, skipping update.")
+        else:
+            update_labels(opex_files, mapping, parsed.dry_run, parsed.output_file)
     else:
         raise Exception("Unsupported action: %s" % parsed.action)
 
