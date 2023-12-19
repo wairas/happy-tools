d180f41da395b0bc86ece3771dd42551f9f65f2a
Peter Reutemann <fracpete@gmail.com>
2023-12-19 15:30:31 +1300

    added ability to limit generated mask to specific list of labels

--- plugins/norm-object-annotations.md
+++ plugins/norm-object-annotations.md
@@ -1,13 +1,13 @@
 # norm-object-annotations
 
-Normalization that only uses pixels from annotations (excl white/black references) to calculate the min/max/range.
+Normalization that only uses pixels from annotations (white/black references are always skipped) to calculate the min/max/range.
 
 ```
 usage: norm-object-annotations [-h] [-V {DEBUG,INFO,WARNING,ERROR,CRITICAL}]
-                               [-A LOGGER_NAME]
+                               [-A LOGGER_NAME] [-l [LABEL [LABEL ...]]]
 
-Normalization that only uses pixels from annotations (excl white/black
-references) to calculate the min/max/range.
+Normalization that only uses pixels from annotations (white/black references
+are always skipped) to calculate the min/max/range.
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -15,4 +15,7 @@ optional arguments:
                         The logging level to use. (default: WARN)
   -A LOGGER_NAME, --logger_name LOGGER_NAME
                         The custom name to use for the logger (default: None)
+  -l [LABEL [LABEL ...]], --label [LABEL [LABEL ...]]
+                        The labels to restrict the calculation to rather than
+                        all (except white/black reference) (default: None)
 ```
--- src/happy/data/normalization/_object_annotations.py
+++ src/happy/data/normalization/_object_annotations.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 
 from PIL import Image, ImageDraw
@@ -11,6 +12,10 @@ class ObjectAnnotationsNormalization(AbstractOPEXAnnotationBasedNormalization):
     Normalization that only uses pixels from annotations (excl white/black references)
     to calculate the min/max/range.
     """
+
+    def __init__(self):
+        super().__init__()
+        self._labels = []
 
     def name(self) -> str:
         """
@@ -28,7 +33,28 @@ class ObjectAnnotationsNormalization(AbstractOPEXAnnotationBasedNormalization):
         :return: the description
         :rtype: str
         """
-        return "Normalization that only uses pixels from annotations (excl white/black references) to calculate the min/max/range."
+        return "Normalization that only uses pixels from annotations (white/black references are always skipped) to calculate the min/max/range."
+
+    def _create_argparser(self) -> argparse.ArgumentParser:
+        """
+        Creates an argument parser.
+
+        :return: the parser
+        :rtype: argparse.ArgumentParser
+        """
+        parser = super()._create_argparser()
+        parser.add_argument("-l", "--label", metavar="LABEL", type=str, help="The labels to restrict the calculation to rather than all (except white/black reference)", required=False, nargs="*")
+        return parser
+
+    def _apply_args(self, ns: argparse.Namespace):
+        """
+        Initializes the object with the arguments of the parsed namespace.
+
+        :param ns: the parsed arguments
+        :type ns: argparse.Namespace
+        """
+        super()._apply_args(ns)
+        self._labels = ns.label
 
     def _do_normalize(self, data):
         """
@@ -40,8 +66,13 @@ class ObjectAnnotationsNormalization(AbstractOPEXAnnotationBasedNormalization):
         # generate mask
         img = Image.new("1", (data.shape[1], data.shape[0]))
         draw = ImageDraw.Draw(img)
+        labels = None
+        if (self._labels is not None) and (len(self._labels) > 0):
+            labels = set(self._labels)
         for obj in self.annotations.objects:
             if (obj.label == LABEL_WHITEREF) or (obj.label == LABEL_BLACKREF):
+                continue
+            if (labels is not None) and (obj.label not in labels):
                 continue
             poly = [tuple(x) for x in obj.polygon.points]
             draw.polygon(poly, fill=1)
