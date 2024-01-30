2c73734f24597a90fa067487b4427d105f57d6a6
Peter Reutemann <fracpete@gmail.com>
2024-01-31 10:00:57 +1300

    removed obsolete MatWriter

--- src/happy/writers/__init__.py
+++ src/happy/writers/__init__.py
@@ -3,7 +3,6 @@ from ._happydata_writer import HappyDataWriter
 from ._csv_training_data_writer import CSVTrainingDataWriter
 from ._envi_writer import EnviWriter
 from ._happy_writer import HappyWriter
-from ._mat_writer import MatWriter
 from ._numpy_writer import NumpyWriter
 from ._matlab_writer import MatlabWriter
 from ._png_writer import PNGWriter
