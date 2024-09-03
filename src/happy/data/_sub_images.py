9505cc2a5ef3a2801005430a448d830266da6879
Peter Reutemann <fracpete@gmail.com>
2024-09-04 08:55:32 +1200

    improved logging messages

--- src/happy/data/_sub_images.py
+++ src/happy/data/_sub_images.py
@@ -50,7 +50,7 @@ def export_sub_images(datamanager: DataManager, path: str, label_regexp: Optiona
         contours = [x.to_absolute(dims[0], dims[1]) for x in matches]
 
     for i, contour in enumerate(contours, start=1):
-        datamanager.log("Exporting %d: %s" % (i, str(contour.label)))
+        datamanager.log("Exporting sub-image #%d: label=%s" % (i, str(contour.label)))
 
         # configure writer
         try:
