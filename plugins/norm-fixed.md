1804e9b87ed40b3c5caf801329e76a3bec36cc59
Peter Reutemann <fracpete@gmail.com>
2024-09-03 16:37:44 +1200

    min/max can be specified per channel now

--- plugins/norm-fixed.md
+++ plugins/norm-fixed.md
@@ -4,7 +4,8 @@ Uses the user-supplied min/max values to normalize the data. Values below or abo
 
 ```
 usage: norm-fixed [-h] [-V {DEBUG,INFO,WARNING,ERROR,CRITICAL}]
-                  [-A LOGGER_NAME] [-m NUM] [-M NUM]
+                  [-A LOGGER_NAME] [-m NUM] [-M NUM] [-r NUM] [-R NUM]
+                  [-g NUM] [-G NUM] [-b NUM] [-B NUM]
 
 Uses the user-supplied min/max values to normalize the data. Values below or
 above get clipped.
@@ -15,6 +16,26 @@ optional arguments:
                         The logging level to use. (default: WARN)
   -A LOGGER_NAME, --logger_name LOGGER_NAME
                         The custom name to use for the logger (default: None)
-  -m NUM, --min NUM     The minimum value to use (default: 0.0)
-  -M NUM, --max NUM     The maximum value to use (default: 10000.0)
+  -m NUM, --min NUM     The minimum value to use, default for all channels
+                        (default: 0.0)
+  -M NUM, --max NUM     The maximum value to use, default for all channels
+                        (default: 10000.0)
+  -r NUM, --red_min NUM
+                        The minimum value to use for the red channel (default:
+                        None)
+  -R NUM, --red_max NUM
+                        The maximum value to use for the red channel (default:
+                        None)
+  -g NUM, --green_min NUM
+                        The minimum value to use for the green channel
+                        (default: None)
+  -G NUM, --green_max NUM
+                        The maximum value to use for the green channel
+                        (default: None)
+  -b NUM, --blue_min NUM
+                        The minimum value to use for the blue channel
+                        (default: None)
+  -B NUM, --blue_max NUM
+                        The maximum value to use for the blue channel
+                        (default: None)
 ```
--- src/happy/data/normalization/_fixed.py
+++ src/happy/data/normalization/_fixed.py
@@ -1,7 +1,7 @@
 import argparse
 import numpy as np
 
-from ._core import AbstractNormalization, channel_to_str
+from ._core import AbstractNormalization, channel_to_str, CHANNEL_RED, CHANNEL_GREEN, CHANNEL_BLUE
 
 
 class FixedNormalization(AbstractNormalization):
@@ -13,6 +13,12 @@ class FixedNormalization(AbstractNormalization):
         super().__init__()
         self._min = None
         self._max = None
+        self._red_min = None
+        self._red_max = None
+        self._green_min = None
+        self._green_max = None
+        self._blue_min = None
+        self._blue_max = None
 
     def name(self) -> str:
         """
@@ -40,8 +46,14 @@ class FixedNormalization(AbstractNormalization):
         :rtype: argparse.ArgumentParser
         """
         parser = super()._create_argparser()
-        parser.add_argument("-m", "--min", metavar="NUM", type=float, help="The minimum value to use", required=False, default=0.0)
-        parser.add_argument("-M", "--max", metavar="NUM", type=float, help="The maximum value to use", required=False, default=10000.0)
+        parser.add_argument("-m", "--min", metavar="NUM", type=float, help="The minimum value to use, default for all channels", required=False, default=0.0)
+        parser.add_argument("-M", "--max", metavar="NUM", type=float, help="The maximum value to use, default for all channels", required=False, default=10000.0)
+        parser.add_argument("-r", "--red_min", metavar="NUM", type=float, help="The minimum value to use for the red channel", required=False, default=None)
+        parser.add_argument("-R", "--red_max", metavar="NUM", type=float, help="The maximum value to use for the red channel", required=False, default=None)
+        parser.add_argument("-g", "--green_min", metavar="NUM", type=float, help="The minimum value to use for the green channel", required=False, default=None)
+        parser.add_argument("-G", "--green_max", metavar="NUM", type=float, help="The maximum value to use for the green channel", required=False, default=None)
+        parser.add_argument("-b", "--blue_min", metavar="NUM", type=float, help="The minimum value to use for the blue channel", required=False, default=None)
+        parser.add_argument("-B", "--blue_max", metavar="NUM", type=float, help="The maximum value to use for the blue channel", required=False, default=None)
         return parser
 
     def _apply_args(self, ns: argparse.Namespace):
@@ -54,6 +66,12 @@ class FixedNormalization(AbstractNormalization):
         super()._apply_args(ns)
         self._min = ns.min
         self._max = ns.max
+        self._red_min = ns.red_min
+        self._red_max = ns.red_max
+        self._green_min = ns.green_min
+        self._green_max = ns.green_max
+        self._blue_min = ns.blue_min
+        self._blue_max = ns.blue_max
 
     def _do_normalize(self, data, channel: int):
         """
@@ -64,12 +82,31 @@ class FixedNormalization(AbstractNormalization):
         :type channel: int
         :return: the normalized data, None if failed to do so
         """
-        data_range = self._max - self._min
-        self.logger().info("channel=%s, min=%f, max=%f, range=%f" % (channel_to_str(channel), self._min, self._max, data_range))
+        # determine min/max
+        _min = self._min
+        _max = self._max
+        if channel == CHANNEL_RED:
+            if self._red_min is not None:
+                _min = self._red_min
+            if self._red_max is not None:
+                _max = self._red_max
+        elif channel == CHANNEL_GREEN:
+            if self._green_min is not None:
+                _min = self._green_min
+            if self._green_max is not None:
+                _max = self._green_max
+        elif channel == CHANNEL_BLUE:
+            if self._blue_min is not None:
+                _min = self._blue_min
+            if self._blue_max is not None:
+                _max = self._blue_max
+
+        data_range = _max - _min
+        self.logger().info("channel=%s, min=%f, max=%f, range=%f" % (channel_to_str(channel), _min, _max, data_range))
 
         if data_range == 0:  # Handle division by zero
             data = np.zeros_like(data)
         else:
-            data = np.clip(data, self._min, self._max)
-            data = (data - self._min) / data_range
+            data = np.clip(data, _min, _max)
+            data = (data - _min) / data_range
         return data
